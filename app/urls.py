aaac215cbbf01a002502f0f99c324a001a03aca4
Liân <42004259+laincs@users.noreply.github.com>
2024-01-23 19:58:17 -0300

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import home, news, news_detail
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import serve
 
 app_name = 'app'
 
 urlpatterns = [
     path('home/', home, name="home"),
     path('news/', news, name='news'),
-    path('news_detail/<int:pk>/', news_detail, name='news_detail')
+    path('news_detail/<int:pk>/', news_detail, name='news_detail'),
+
+    re_path(r'^(?!static/|media/|news_images).*$', RedirectView.as_view(url='/home/'))
+
 ]
