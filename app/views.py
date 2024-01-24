93ba5c2e3166aeb44b6b82c5a1473952f422c877
Liân <42004259+laincs@users.noreply.github.com>
2024-01-23 22:02:09 -0300

    Update

--- app/views.py
+++ app/views.py
@@ -3,13 +3,10 @@ from django.shortcuts import render, get_object_or_404
 from .models import News
 
 def home(request):
-    # Obtener las últimas noticias (ajusta según tu lógica)
-    latest_news = News.objects.all()[:5]  # Obtener las últimas 3 noticias
+    latest_news = News.objects.all()[:5] 
 
-    # Imprimir para verificar en la consola de desarrollo
     print("Latest News:", latest_news)
 
-    # Pasar las noticias a la plantilla
     context = {
         'latest_news': latest_news,
     }
